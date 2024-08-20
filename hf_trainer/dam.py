bug fixes, added sequence length normalization to kl_div, changed default training hyperparameters

hf_trainer/dam.py
@@ -48,11 +48,11 @@ class DAMBaseLayer(nn.Module):
         similarity_loss = torch.tensor(0.0)
 
         if lambda_coef is not None:
-            sim_12 = F.cosine_similarity(self.merger_1, self.merger_2, dim=0)
-            sim_13 = F.cosine_similarity(self.merger_1, self.merger_3, dim=0)
-            sim_23 = F.cosine_similarity(self.merger_2, self.merger_3, dim=0)
+            sim_12 = F.cosine_similarity(self.merger_1, self.merger_2, dim=0).to(similarity_loss.device)
+            sim_13 = F.cosine_similarity(self.merger_1, self.merger_3, dim=0).to(similarity_loss.device)
+            sim_23 = F.cosine_similarity(self.merger_2, self.merger_3, dim=0).to(similarity_loss.device)
             
-            similarity_loss = (sim_12 + sim_13 + sim_23) / 3
+            similarity_loss += (sim_12 + sim_13 + sim_23) / 3
             similarity_loss *= lambda_coef
 
         return similarity_loss
@@ -60,7 +60,7 @@ class DAMBaseLayer(nn.Module):
     def compute_mergers_L2_reg(self, lambda_coef_reg=None):
         l2_reg = torch.tensor(0.0)
         if lambda_coef_reg is not None:
-            l2_reg = (
+            l2_reg += (
                 self.merger_1.norm(2) +
                 self.merger_2.norm(2) +
                 self.merger_3.norm(2)

hf_trainer/dam_trainer.py
@@ -6,13 +6,15 @@ import os
 from tqdm import tqdm
 
 class DAMTrainer(Trainer):
-    def compute_loss(self, model, inputs, lambda_coef=0.01, lambda_coef_reg=None, temperature=2.0, return_outputs=False):
-        # Ensure all tensors are on the same device (single GPU)
-        device = model.device
-        
+    def __init__(self, lambda_coef=0.01, lambda_coef_reg=None, temperature=2.0, **kwargs):
+        super().__init__(**kwargs)
         self.lambda_coef = lambda_coef
         self.lambda_coef_reg = lambda_coef_reg
         self.temperature = temperature
+    
+    def compute_loss(self, model, inputs, return_outputs=False):
+        # Ensure all tensors are on the same device (single GPU)
+        device = model.device
 
         # Extract inputs for model and move them to the correct device
         input_ids_1 = inputs['input_ids_1'].to(device)
@@ -57,33 +59,31 @@ class DAMTrainer(Trainer):
 
         loss_1 = F.kl_div(
             F.log_softmax(merged_logits_1 / self.temperature, dim=-1),
-            F.softmax(logits_1 / temperature, dim=-1),
+            F.softmax(logits_1 / self.temperature, dim=-1),
             reduction='batchmean'
-        ) * (self.temperature ** 2)
+        ) * (self.temperature ** 2) / input_ids_1.size(1)
 
         loss_2 = F.kl_div(
             F.log_softmax(merged_logits_2 / self.temperature, dim=-1),
             F.softmax(logits_2 / self.temperature, dim=-1),
             reduction='batchmean'
-        ) * (self.temperature ** 2)
+        ) * (self.temperature ** 2) / input_ids_2.size(1)
 
         loss_3 = F.kl_div(
             F.log_softmax(merged_logits_3 / self.temperature, dim=-1),
             F.softmax(logits_3 / self.temperature, dim=-1),
             reduction='batchmean'
-        ) * (self.temperature ** 2)
+        ) * (self.temperature ** 2) / input_ids_3.size(1)
 
         similarity_loss = torch.tensor(0.0, device=device)
         similarity_reg_loss = torch.tensor(0.0, device=device)
-        for module in base_model.modules():
+        for module in model.modules():
             if isinstance(module, (DAMLinearLayer, DAMEmbeddingLayer)):
                 similarity_loss += module.compute_mergers_similarity(self.lambda_coef).to(similarity_loss.device)
                 similarity_reg_loss += module.compute_mergers_L2_reg(self.lambda_coef_reg).to(similarity_reg_loss.device)
          
         # Total loss (sum across the batch)
         total_loss = (loss_1 + loss_2 + loss_3) + similarity_loss + similarity_reg_loss
-
-        print(total_loss)
 
         return (total_loss, merged_logits_1) if return_outputs else total_loss
 

hf_trainer/model_preparation.py
@@ -1,9 +1,26 @@
 import torch
+import gc
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from glom import glom, Assign
 from dam import DAMLinearLayer, DAMEmbeddingLayer
 from utils import find_linear_layers, find_embedding_layers
 
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for module in model.modules():
+        for param in module.parameters():        
+            all_param += param.numel()
+            
+            if param.requires_grad:
+                trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+    
 def prepare_model(MODEL_ID, MODEL_ID_A, MODEL_ID_B, MODEL_ID_C, apply_to_embeddings=True):
     # Load models
     base_model = AutoModelForCausalLM.from_pretrained(MODEL_ID, torch_dtype=torch.bfloat16, device_map="auto")
@@ -17,48 +34,37 @@ def prepare_model(MODEL_ID, MODEL_ID_A, MODEL_ID_B, MODEL_ID_C, apply_to_embeddi
 
     tokenizer = AutoTokenizer.from_pretrained(MODEL_ID, use_fast=True)
 
-    # Truncate model_C's layers to match the base model's vocabulary size
-    truncated_lm_head = torch.nn.Linear(in_features=4096, out_features=32000, bias=False)
-    truncated_embed = torch.nn.Embedding(num_embeddings=32000, embedding_dim=4096)
-
-    with torch.no_grad():
-        truncated_lm_head.weight.data = model_C.lm_head.weight.data[:32000, :]
-        truncated_embed.weight.data = model_C.model.embed_tokens.weight.data[:32000, :]
-
-    assign_embed = Assign('model.embed_tokens', truncated_embed)
-    assign_lm_head = Assign('lm_head', truncated_lm_head)
-    glom(model_C, assign_embed)
-    glom(model_C, assign_lm_head)
-
     # Prepare DAM layers
     linear_modules = find_linear_layers(base_model)
     embedding_modules = find_embedding_layers(base_model) if apply_to_embeddings else []
-
-    for m in linear_modules + embedding_modules:
+    
+    for m in (linear_modules + embedding_modules):
         module_a = glom(model_A, m)
         module_b = glom(model_B, m)
         module_c = glom(model_C, m)
-
+    
+        dam_layer = None
+        
         if isinstance(module_a, torch.nn.Embedding):
             dam_layer = DAMEmbeddingLayer(
                 embedding_a=module_a,
                 embedding_b=module_b,
                 embedding_c=module_c,
-                device=base_model.device,
+                device=module_c.weight.device,
                 dtype=module_a.weight.dtype
             )
         else:
             dam_layer = DAMLinearLayer(
-                base_linear=glom(base_model, m),
                 linear_a=module_a,
                 linear_b=module_b,
                 linear_c=module_c,
-                device=base_model.device,
+                device=module_a.weight.device,
                 dtype=module_a.weight.dtype
             )
-
-        assign = Assign(m, dam_layer)
-        glom(base_model, assign)
+    
+        if dam_layer is not None:
+            assign = Assign(m, dam_layer)
+            glom(base_model, assign)
 
     # Freeze all parameters except for the specific layers
     for module in base_model.modules():
@@ -68,5 +74,19 @@ def prepare_model(MODEL_ID, MODEL_ID_A, MODEL_ID_B, MODEL_ID_C, apply_to_embeddi
         else:
             for param in module.parameters():
                 param.requires_grad = False
+
+    print_trainable_parameters(base_model)
+
+
+    # We no longer need the weight source models, this will free up VRAM for training
+
+    model_A.cpu()
+    model_B.cpu()
+    model_C.cpu()
+    
+    del model_A, model_B, model_C
+    gc.collect()
+    torch.cuda.empty_cache()
+
 
     return base_model, tokenizer