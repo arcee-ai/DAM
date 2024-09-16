a873e925813db3751f4efb1c5315f5111c6ff286
ElliotStein <Elliot.stein17@gmail.com>
2024-09-16 13:38:52 +0000

    fix bash

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,27 +2,27 @@
 
 # Experiment 1: Running with default parameters
 echo "Experiment 1: Running with default parameters: KL + similarity."
-python train.py \
+python dam/train_dam.py \
 
 echo "Experiment 2: KL Only."
-python train.py \
+python dam/train_dam.py \
     --similarity=False
 
 echo "Experiment 3: Enabling 'l1_l2_reg' loss function."
-python train.py \
+python dam/train_dam.py \
     --l1_l2_reg=True 
 
 echo "Experiment 4: Replace 'similarity' with 'overlap' loss function."
-python train.py \
+python dam/train_dam.py \
     --overlap=True \
     --similarity=False
 
 echo "Experiment 5: Replacing 'kl' with 'mse'."
-python train.py \
+python dam/train_dam.py \
     --kl=False \
     --mse=True
 
 echo "Experiment 6: Replacing 'kl' with 'entropy'."
-python train.py \
+python dam/train_dam.py \
     --kl=False \
     --entropy=True
